ac0337ecdf53b906a52f5800692cf3a9295f1f3e
ambik <kdhakshinamoorthy@csus.edu>
2026-01-21 22:37:06 -0800

    BrowserRouter

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,21 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./app/App.tsx";
+import "./styles/index.css";
+import "./styles/app.css";
 
-  import { createRoot } from "react-dom/client";
-  import App from "./app/App.tsx";
-  import "./styles/index.css";
-  import "./styles/app.css";
+const params = new URLSearchParams(window.location.search);
+const redirectPath = params.get("p");
 
-  createRoot(document.getElementById("root")!).render(<App />);
-  
+if (redirectPath) {
+  window.history.replaceState(null, "", redirectPath);
+}
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
